40a4753a45f018acfd6119fa5a10bc7a3aa7860e
scorost1 <scorost21tmv@mail.ru>
2024-03-02 16:38:27 +0300

    Астро Фото без проверки даты

--- bot.py
+++ bot.py
@@ -13,21 +13,65 @@ with open('token.txt') as f:
 # response = requests.get(endpoint, params=param).json()['result']
 # pprint.pprint(response)
 
-offset = -2
+def givePhoto(date: str) -> (str, str):
+    endpoint = 'https://api.nasa.gov/planetary/apod'
+    params = {'api_key': 'DEMO_KEY', 'date': date}
+    res = requests.get(endpoint, params=params).json()
+    explanation = res['explanation']
+    urlPhoto = res['url']
+    return (urlPhoto, explanation)
+
+
+
+# def numDay(month):
+#     if month == '2' and int(year) % 4 == 0 and int(year) % 100 != 0 or int(year) % 400 == 0:
+#         return '29'
+#     elif month == '2' and int(year) % 4 != 0 and int(year) % 100 == 0 or int(year) % 400 != 0:
+#         return '28'
+#     elif month == '4' or month == '6' or month == '9' or month == '11':
+#         return '30'
+#     else:
+#         return '31'
+#
+#
+#
+#
+# def chekDate(date: str) -> bool:
+#   d = (dataX.split('-'))
+#   year = d[0]
+#   if len(year) != 4 or int(year) > 2024 or int(year) < 1980:
+#       return False
+#   month = d[1]
+#   if len(month) == 0 or len(month) > 2 or int(month) < 1 or int(month) > 12:
+#       return False
+#   day = d[2]
+#   if len(day) == 0 or len(day) > 2 or int(day) < 1 or int(day) > int(numDay(month)):
+#       return False
+
+
+
 import time
+
+offset = -2
 while True:
     endpoint = f'https://api.telegram.org/bot{token}/getUpdates'
-    param = {'timeout': 60, 'offset': offset + 1}
+    param = {'offset': offset + 1}
     response = requests.get(endpoint, params=param).json()
     if response['result']:
         offset = response['result'][0]['update_id']
         userText = response['result'][0]['message']['text']
         chatID = response['result'][0]['message']['chat']['id']
-        pprint.pprint(response)
+
+        photoUrl, photoExp = givePhoto(userText)
+        pprint.pprint(chatID)
+        endpoint = f'https://api.telegram.org/bot{token}/sendPhoto'
+        params = {'chat_id': chatID, 'photo': photoUrl}
+        resul = requests.get(endpoint, params=params)
 
         endpoint = f'https://api.telegram.org/bot{token}/sendMessage'
-        params = {'chat_id': chatID, 'text': userText}
+        params = {'chat_id': chatID, 'text': photoExp}
         res = requests.get(endpoint, params=params)
+        pprint.pprint(res)
     time.sleep(1)
 
 
